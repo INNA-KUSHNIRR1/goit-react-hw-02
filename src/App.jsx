8c301a7b722969af337f8e7710f821287872e52d
INNA-KUSHNIRR1 <149315820+INNA-KUSHNIRR1@users.noreply.github.com>
2024-05-17 13:21:32 +0200

    add components

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import "./App.css";
+import Description from "./components/Description/Description";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+
+function App() {
+  // const [count, setCount] = useState({
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // });
+
+  return (
+    <>
+      <Description />
+      <Options />
+      <Feedback />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,10 @@
+const Options = () => {
+  return (
+    <div>
+      <button></button>
+      <button></button>
+      <button></button>
+    </div>
+  );
+};
+export default Options;
